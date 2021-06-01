449b2b6710fdf7098fc0ff907cd48f06c268d36f
simplon <basilesimplon@gmail.com>
2021-06-01 09:33:01 +0200

    cagnot'pote

---
+++ src/Entity/Participant.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ParticipantRepository::class)
+ */
+class Participant
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $email;
+
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_anonymous;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Campaign::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $campaign_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+
+    public function getIsAnonymous(): ?bool
+    {
+        return $this->is_anonymous;
+    }
+
+    public function setIsAnonymous(bool $is_anonymous): self
+    {
+        $this->is_anonymous = $is_anonymous;
+
+        return $this;
+    }
+
+    public function getCampaignId(): ?Campaign
+    {
+        return $this->campaign_id;
+    }
+
+    public function setCampaignId(?Campaign $campaign_id): self
+    {
+        $this->campaign_id = $campaign_id;
+
+        return $this;
+    }
+}
---
+++ src/Form/CampaignType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campaign;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CampaignType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre de votre campagne'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Décrivez à vos amis pourquoi vous faites une campagne'
+            ])
+            ->add('goal', NumberType::class, [
+                'label' => 'Votre objectif en euros'
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Votre nom'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Campaign::class,
+        ]);
+    }
+}
